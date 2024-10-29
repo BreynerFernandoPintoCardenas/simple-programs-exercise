739f6b499c29032f688f3dc0a2a86aecfb39a663
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2024-10-29 13:40:55 -0500

    solucion ejercicio Decimales

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-#Escriba un programa que pregunte al usuario la hora actual t del reloj y un número entero de horas h, que indique qué hora marcará el reloj dentro de h horas:
+#Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
 
-#Hora actual: 3
-#Cantidad de horas: 5
-#En 5 horas, el reloj marcara las 8
-#Hora actual: 11
-#Cantidad de horas: 43
-#En 43 horas, el reloj marcara las 6
+#Ingrese un numero: 4.5
+#0.5
+#Ingrese un numero: -1.19
+#0.19
 
-hourActual=int(input("Enter the actual hour: "))
-hourFuture=int(input("Enter a hour in the future: "))
-hourSum=(hourActual+hourFuture)
-print(f"In {hourFuture} the clock will show {hourSum}")
+number=float(input("enter a number with decimals: "))
+numberN=number - int(number)
+print(numberN)
